67ffb53638f675455820cffab7899635ffa3a121
kiss4437 <16162839@qq.com>
2018-05-01 11:25:56 +0800

    Add files via upload

---
+++ drcom_init.d
@@ -0,0 +1,25 @@
+#!/bin/sh /etc/rc.common
+# Copyright (C) 2006-2011 OpenWrt.org
+
+
+. /lib/functions.sh
+. /lib/functions/network.sh
+
+START=25
+
+start() {
+    service_start /usr/Drcom/drcom -m dhcp -c /usr/Drcom/drcom.conf -d -e
+}
+
+stop() {
+    service_stop /usr/Drcom/drcom
+}
+
+restart() {
+    stop && start
+}
+
+
+#/etc/init.d/drcom start
+#/etc/init.d/drcom enable
+#/etc/init.d/network 本文件注释第29行setup_switch
